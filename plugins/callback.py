04831fd16a4379b281c9f00589ba7fd5f244f743
kalitg028 <Amazonbotmv@gmail.com>
2026-06-03 13:24:51 +0530

    Comment out referral button in callback.py
    Commented out the referral button in the inline keyboard.

--- plugins/callback.py
+++ plugins/callback.py
@@ -49,10 +49,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
            # InlineKeyboardButton('✨ ʙᴜʏ ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ : ʀᴇᴍᴏᴠᴇ ᴀᴅꜱ ✨', callback_data="premium_info")
         ],
             [
-                InlineKeyboardButton(
-                    '🎁 ʀᴇғᴇʀ & ᴇᴀʀɴ 🎁',
-                    callback_data="reffff"
-                )
+             #   InlineKeyboardButton(
+             #       '🎁 ʀᴇғᴇʀ & ᴇᴀʀɴ 🎁',
+             #       callback_data="reffff"
+             #   )
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
