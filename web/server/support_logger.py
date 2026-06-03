b6130611de838b3870b8e03e886d54c35832b2c2
kalitg028 <Amazonbotmv@gmail.com>
2026-06-03 10:31:52 +0530

    Update support contact link in support_logger.py

--- web/server/support_logger.py
+++ web/server/support_logger.py
@@ -2,7 +2,7 @@ import logging
 import sys
 import traceback
 
-SUPPORT = "https://t.me/AV_SUPPORT_GROUP"
+SUPPORT = "https://t.me/Indian_MV_Group"
 
 # ANSI Colors for Terminal Highlighting
 class Colors:
